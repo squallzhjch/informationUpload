c4656c8737279316df16fb062b30e22b2524e727
chentao0119 <95798268@qq.com>
2015-12-09 14:08:45 +0800

    chongtu

--- InformationUpload/src/com/informationUpload/Activity/MyApplication.java
+++ InformationUpload/src/com/informationUpload/Activity/MyApplication.java
@@ -3,6 +3,7 @@ package com.informationUpload.Activity;
 import android.app.Application;
 import android.util.DisplayMetrics;
 
+import com.baidu.mapapi.SDKInitializer;
 import com.informationUpload.VoiceSpeech.VoiceSpeechManager;
 import com.informationUpload.fragments.utils.IntentHelper;
 import com.informationUpload.tool.ImageTool;
@@ -16,15 +17,16 @@ import com.informationUpload.tool.ImageTool;
  */
 public class MyApplication extends Application{
 
-    private VoiceSpeechManager mVoiceSpeechManager;
+	private VoiceSpeechManager mVoiceSpeechManager;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		// 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
+		SDKInitializer.initialize(this);
+		mVoiceSpeechManager = VoiceSpeechManager.getInstance();
+		mVoiceSpeechManager.init(this);
 
-        mVoiceSpeechManager = VoiceSpeechManager.getInstance();
-        mVoiceSpeechManager.init(this);
-
-        IntentHelper.getInstance().init(this);
-    }
+		IntentHelper.getInstance().init(this);
+	}
 }
