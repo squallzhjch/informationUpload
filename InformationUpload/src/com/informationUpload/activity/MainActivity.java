7c24a644282bdf664a0c4ebe3e7650d1730d248d
squallzhjch <zhangjingchao@navinfo.com>
2016-01-25 15:28:41 +0800

    修改升级模块

--- InformationUpload/src/com/informationUpload/activity/MainActivity.java
+++ InformationUpload/src/com/informationUpload/activity/MainActivity.java
@@ -23,8 +23,7 @@ public class MainActivity extends BaseActivity implements ActivityInstanceStateL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UmengUpdateAgent.setUpdateOnlyWifi(false);
-		UmengUpdateAgent.update(this);
+
         mOnSaveInstanceStateInvoked = false;
         if (savedInstanceState == null) {
             setContentView(R.layout.activity_main);
--- InformationUpload/src/com/informationUpload/activity/WelcomeActivity.java
+++ InformationUpload/src/com/informationUpload/activity/WelcomeActivity.java
@@ -1,30 +1,64 @@
 package com.informationUpload.activity;
 
 import com.informationUpload.R;
+import com.umeng.update.UmengDialogButtonListener;
 import com.umeng.update.UmengUpdateAgent;
+import com.umeng.update.UmengUpdateListener;
+import com.umeng.update.UpdateResponse;
+import com.umeng.update.UpdateStatus;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.animation.AccelerateDecelerateInterpolator;
 
 public class WelcomeActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcomepage);
-		
-		new Handler().postDelayed(new Runnable() {
 
+		UmengUpdateAgent.setUpdateOnlyWifi(false);
+		UmengUpdateAgent.setUpdateAutoPopup(false);
+		UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
 			@Override
-			public void run() {
+			public void onUpdateReturned(int i, UpdateResponse updateResponse) {
+				switch (i) {
+					case UpdateStatus.Yes:
+						UmengUpdateAgent.showUpdateDialog(WelcomeActivity.this, updateResponse);
+						break;
+					default:
+						new Handler().postDelayed(new Runnable() {
 
-				startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
-				WelcomeActivity.this.finish();
+							@Override
+							public void run() {
+								startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+								WelcomeActivity.this.finish();
+							}
+						}, 1000);
+						break;
+				}
 			}
-		}, 2000);
+		});
+		UmengUpdateAgent.update(this);
+		UmengUpdateAgent.setDialogListener(new UmengDialogButtonListener() {
+			@Override
+			public void onClick(int i) {
+				switch (i) {
+					case UpdateStatus.Update:
+						break;
+					default:
+						new Handler().postDelayed(new Runnable() {
 
+							@Override
+							public void run() {
+								startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+								WelcomeActivity.this.finish();
+							}
+						}, 1000);
+						break;
+				}
+			}
+		});
 	}
 }
